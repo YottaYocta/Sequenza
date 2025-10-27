e6760fd012e890c114212567adf5d832d9f79d53
yottayocta <chubbean.rq@gmail.com>
2025-10-27 15:52:49 -0400

    task: specs for app

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  type MidPassFilter = {
+    // filter value that returns true for all values in between low and high and false otherwise (low and high are clamped to 0 to 1)
+    low: number;
+    high: number;
+  };
+
+  interface DotsSettings {
+    offsetX: number;
+    offsetY: number;
+    dotRadius: number;
+    gapX: number; // horizontal separation
+    gapY: number; // vertical separation
+    borderRadius: number; // clamped to -1 to 1, with 0 being no corners (creates pixels) and 1 being equal to the dotRadius (fully rounded). -1 corresponds to a star shape (four pointed star; a circle but with edges concave, leaving 4 points)
+    // controls the rotation of each Dot individually
+    rotation: number;
+  }
 </script>
 
 <main
@@ -10,8 +23,11 @@
   <div
     class="w-full h-full bg-neutral-100 border border-neutral-50 rounded-sm flex flex-col p-4 gap-4"
   >
+    <!-- intially, this should be a div with the text 'upload image' in the middle -->
+    <!-- uploading an image should display it in the canvas -->
     <canvas class="w-full h-full bg-blue-600"></canvas>
     <div class="w-full p-4 flex gap-4 h-24">
+      <!-- these controls should allow the user to modify the image (source still maintained in memory). if somethign is changed, show a button with text 'revert' -->
       <div class="w-36">
         <label for="hue">Hue</label>
         <input
@@ -53,14 +69,18 @@
   <div
     class="w-full h-full bg-neutral-100 border border-neutral-50 rounded-sm flex flex-col p-4 gap-4"
   >
+    <!-- this field should use the modified image to generate a matching svg-->
     <svg class="w-full h-full bg-blue-600"></svg>
     <div class="h-24 w-full flex flex-col gap-4">
       <p>Effects</p>
       <ul class="flex gap-4">
         <!-- make selection; selecting one of these will affect what is rendered -->
+        <!-- choosing dots will open up a menu in the the top right hand side of the canvas (use abs pos to overlay) that controls settings. the algorithm is as follows. use the size-->
         <li>Dots</li>
-        <li>Pixels</li>
-        <li>Dither</li>
+        <!-- future features; ignore for now -->
+        <li>Bands</li>
+        <li>Particles</li>
+        <li>Ascii</li>
       </ul>
     </div>
   </div>
