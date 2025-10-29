55ef751b40dff24d79b494df5a0df5fde2d1fb89
yottayocta <chubbean.rq@gmail.com>
2025-10-29 01:17:10 -0400

    task: verify that separation of states work as expected

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import DraggableContainer from "./components/DraggableContainer.svelte";
 
   type Node = string;
 
   let nodes: Node[] = $state(["FirstNode", "SecondNode"]);
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      nodes[0] = Date.now().toString();
+      nodes[1] = Date.now().toString();
+    });
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
 {#each nodes as node}
