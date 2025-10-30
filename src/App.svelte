e84f0891df13d75069479060cc31cc53ac36e737
yottayocta <chubbean.rq@gmail.com>
2025-10-30 01:53:08 -0400

    style: source node

--- src/App.svelte
+++ src/App.svelte
@@ -275,7 +275,7 @@
     </DraggableContainer>
 
     {#each processingPipeline as node, idx}
-      <DraggableContainer startY={idx === 0 ? 200 : 100 + 400 * idx}>
+      <DraggableContainer startY={idx === 0 ? 200 : 100 + 320 * idx}>
         {#snippet children()}
           {#if isAdjustmentNode(node)}
             <AdjustmentNode
@@ -295,7 +295,7 @@
       <DraggableContainer
         mergedMode={true}
         startX={500}
-        startY={400 * idx + 100}
+        startY={100 + 320 * idx}
       >
         {#snippet children()}
           <ViewerNode
--- src/components/SourceNode.svelte
+++ src/components/SourceNode.svelte
@@ -70,38 +70,42 @@
   }
 </script>
 
-<div class="flex items-center gap-2">
-  <div class="flex flex-col gap-1">
-    <h3 class="font-semibold">Source Image</h3>
-
-    <input
-      type="file"
-      accept="image/*"
-      bind:this={fileInputRef}
-      onchange={handleFileUpload}
-      class="hidden"
-    />
-
-    <div class="border border-neutral-300 p-2 bg-neutral-50">
-      <canvas {@attach canvasAttachment} class="w-24 h-auto"></canvas>
-    </div>
-
-    {#if currentImageData}
-      <div class="text-xs text-neutral-500">
-        Size: {currentImageData.width} × {currentImageData.height}
-      </div>
-    {:else}
-      <div class="text-xs text-neutral-500">No image loaded</div>
-    {/if}
-  </div>
-
-  <button
-    onclick={triggerFileInput}
-    class="w-min h-min text-nowrap text-sm border border-neutral-900 hover:bg-neutral-200 active:bg-black active:text-white relative"
-  >
-    {currentImageData ? "Change Image" : "Upload Image"}
+<div class="flex items-center gap-2 py-3">
+  <div class="border p-2 bg-white relative">
+    <canvas {@attach canvasAttachment} class="w-24 h-auto"> </canvas>
+    <span class="absolute right-0 bottom-0 w-2 h-2 bg-black translate-1/2">
+    </span>
     <div class="absolute right-0 bottom-0">
       <Endpoint nodeIdx={0} type="end"></Endpoint>
     </div>
-  </button>
+  </div>
+
+  <div class="h-full flex flex-col gap-1">
+    <div class="flex flex-col justify-start h-full">
+      <h3 class="font-semibold">Source Image</h3>
+
+      <input
+        type="file"
+        accept="image/*"
+        bind:this={fileInputRef}
+        onchange={handleFileUpload}
+        class="hidden"
+      />
+
+      {#if currentImageData}
+        <p>
+          {currentImageData.width} x {currentImageData.height}
+        </p>
+      {:else}
+        <div class="text-xs text-neutral-500">No image loaded</div>
+      {/if}
+    </div>
+
+    <button
+      onclick={triggerFileInput}
+      class="button-1 outline hover:bg-neutral-200 relative"
+    >
+      {currentImageData ? "Change Image" : "Upload Image"}
+    </button>
+  </div>
 </div>
