2118d5ce9164ae7ce745299870cb9a88faad8262
yottayocta <chubbean.rq@gmail.com>
2025-11-27 18:43:09 -0500

    feat: session logins

--- src/lib/server/util.ts
+++ src/lib/server/util.ts
@@ -2,8 +2,10 @@ import type { Cookies } from '@sveltejs/kit';
 import { Session } from './models/Session';
 import { dev } from '$app/environment';
 import type { ObjectId } from 'mongoose';
+import { connectDB } from './db';
 
 export const registerSessionToken = async (userID: ObjectId, cookies: Cookies): Promise<void> => {
+	await connectDB();
 	const sessionToken = crypto.randomUUID();
 
 	const maxAge = 1 * 1 * 5 * 60 * 1000; // five minutes
@@ -22,4 +24,30 @@ export const registerSessionToken = async (userID: ObjectId, cookies: Cookies): 
 		secure: !dev,
 		maxAge: maxAge
 	});
+};
+
+export const validateSessionToken = async (sessionToken: string): Promise<boolean> => {
+	try {
+		await connectDB();
+		const matchingSessionToken = await Session.findOne({ sessionToken: sessionToken }).lean();
+
+		// session not found
+		if (matchingSessionToken === null) {
+			return false;
+		}
+
+		// session found
+
+		// expired
+		if (matchingSessionToken.expiresAt.getTime() < Date.now()) {
+			await Session.deleteOne({ sessionToken: sessionToken });
+
+			return false;
+		}
+	} catch (error) {
+		console.error('Error in main layout:', error);
+		return false;
+	}
+
+	return true;
 };
