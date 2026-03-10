c456572a8ea0c5d17e922c6622a961521a3d8242
yottayocta <chubbean.rq@gmail.com>
2026-03-10 18:55:36 -0400

    task: prepare for publish

--- local/src/index.ts
+++ local/src/index.ts
@@ -2,14 +2,4 @@ import './index.css';
 
 export { Renderer } from './lib/renderer';
 export type { Shader, Uniforms, TextureUniform, Connection, Patch } from './lib/renderer';
-
-export { Editor } from './components/Editor';
-export { EditorContext } from './components/EditorContext';
-export { ShaderNode } from './components/ShaderNode';
-export type { ShaderNodeData } from './components/ShaderNode';
 export { RendererComponent } from './components/RendererComponent';
-export { Scrubber } from './components/Scrubber';
-export { default as UniformForm } from './components/UniformForm';
-export { default as CustomHandle } from './components/CustomHandle';
-export { extractFields } from './lib/Field';
-export type { Field } from './lib/Field';
