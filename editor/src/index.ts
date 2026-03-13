b2fb3f0e942a035324dbb8edb77ad9bd5f66000c
yottayocta <chubbean.rq@gmail.com>
2026-03-13 01:34:15 -0400

    task: updated exports

--- editor/src/index.ts
+++ editor/src/index.ts
@@ -1,2 +1,26 @@
-export { Renderer, RendererComponent, extractFields, exportSequenzaPatch } from "@yottayocta/sequenza";
-export type { Shader, Uniforms, TextureUniform, Connection, Patch, Field } from "@yottayocta/sequenza";
+export {
+  Renderer,
+  RendererComponent,
+  extractFields,
+  exportSequenzaPatch,
+} from "@yottayocta/sequenza";
+export type {
+  Shader,
+  Uniforms,
+  TextureUniform,
+  Connection,
+  Patch,
+  Field,
+} from "@yottayocta/sequenza";
+export { Editor } from "./components/Editor";
+export { Dialog } from "./components/Dialog";
+
+import CustomHandle from "./components/CustomHandle";
+export { CustomHandle };
+
+export { EditorContext } from "./components/EditorContext";
+export { Scrubber } from "./components/Scrubber";
+
+export { ShaderNode } from "./components/ShaderNode";
+import UniformForm from "./components/UniformForm";
+export { UniformForm };
