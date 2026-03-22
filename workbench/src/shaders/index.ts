e5b5e0dd7680f65562d3f514e49d7ba92c950322
yottayocta <chubbean.rq@gmail.com>
2026-03-22 06:21:36 -0400

    task: remove unused shaders, add to registry

--- workbench/src/shaders/index.ts
+++ workbench/src/shaders/index.ts
@@ -3,11 +3,9 @@ import daffodil from "../../shaders/daffodil.frag?raw";
 import dither_bayer from "../../shaders/dither_bayer.frag?raw";
 import dots from "../../shaders/dots.frag?raw";
 import diamonds from "../../shaders/diamonds.frag?raw";
-import gaussian from "../../shaders/gaussian.frag?raw";
 import gradientmap from "../../shaders/gradientmap.frag?raw";
 import hsl from "../../shaders/hsl.frag?raw";
 import imageViewer from "../../shaders/imageViewer.frag?raw";
-import linear from "../../shaders/linear.frag?raw";
 import lines from "../../shaders/lines.frag?raw";
 import rgb from "../../shaders/rgb.frag?raw";
 import add from "../../shaders/add.frag?raw";
@@ -15,6 +13,10 @@ import subtract from "../../shaders/subtract.frag?raw";
 import mix from "../../shaders/mix.frag?raw";
 import multiply from "../../shaders/multiply.frag?raw";
 import divide from "../../shaders/divide.frag?raw";
+import radial_blur from "../../shaders/radial_blur.frag?raw";
+import directional_blur from "../../shaders/directional_blur.frag?raw";
+import noise from "../../shaders/noise.frag?raw";
+import dispersion from "../../shaders/dispersion.frag?raw";
 
 export const staticShaders: Record<string, string> = {
   key: chromakey,
@@ -22,11 +24,13 @@ export const staticShaders: Record<string, string> = {
   dots: dots,
   daffodil: daffodil,
   diamonds: diamonds,
-  "blur (gaussian)": gaussian,
   "gradient map": gradientmap,
   "hue/saturation/value": hsl,
   "image viewer": imageViewer,
-  "blur (linear)": linear,
+  "blur (radial)": radial_blur,
+  "blur (linear)": directional_blur,
+  noise: noise,
+  dispersion: dispersion,
   lines: lines,
   "color adjust": rgb,
   "mix (add)": add,
