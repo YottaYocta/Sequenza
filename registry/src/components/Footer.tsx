07cd81a8248b6c34154aa69308e23b8bb3394530
yottayocta <chubbean.rq@gmail.com>
2026-04-20 05:55:15 -0400

    style: footer width

--- registry/src/components/Footer.tsx
+++ registry/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between w-full max-w-screen-2xl ">
+    <footer className="flex items-center justify-between w-full max-w-6xl ">
       <div className="flex items-center gap-8">
         <div className="flex items-center gap-1.5 opacity-60  ">
           <span>2025-2026</span>
@@ -11,6 +11,9 @@ export function Footer() {
         <span className="opacity-60  ">GPL3 License</span>
       </div>
       <div className="flex items-center gap-9">
+        <Link to="/editor" className="opacity-60 hover:opacity-80 transition">
+          Editor
+        </Link>
         <a
           href="https://github.com/YottaYocta/Sequenza"
           className="opacity-60   hover:opacity-80 "
